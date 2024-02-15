ee5142cbefe49fe8251ef21f24cfec839e4a912f
Artem <atremat@gmail.com>
2024-02-15 21:27:12 +0200

    rename functions

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import axios from 'axios';
 
-let page;
-let searchText = '';
-let totalPages;
-let gallery = null;
+let page; //current page
+let searchText = ''; //getting from input
+let totalPages; //total number of pages; using it for more button
+let gallery = null; //simplelightbox instance
 
 const refs = {
   form: document.querySelector('.form'),
@@ -47,18 +47,28 @@ async function onSearch(e) {
   //show css-loader
   loaderShow(refs.searchLoader);
 
-  //value of input
+  //getting value of input
   searchText = e.currentTarget.elements.query.value;
 
   try {
-    const data = await fetchPhotos(searchText);
+    //getting images
+    const data = await getImages(searchText);
+
+    //calculating total nunmber of pages
+    totalPages = Math.ceil(data.total / 15);
+
+    //if we're not on last page( there are more pages except current page) , then show more button
+    moreBtnCheck();
+
     createGallery(data);
   } catch (err) {
     console.log(err);
   }
   refs.form.reset();
 }
-async function fetchPhotos(searchText) {
+
+//getting images
+async function getImages(searchText) {
   //parameters of searching query
   const searchParams = new URLSearchParams({
     key: '42031589-0742425241f8784341d5a922f',
@@ -69,36 +79,30 @@ async function fetchPhotos(searchText) {
     per_page: 15,
     page,
   });
+
   const response = await axios.get(`https://pixabay.com/api/?${searchParams}`);
   const data = response.data;
-  totalPages = Math.ceil(data.total / 15);
-  console.log(data.total);
-  console.log('total pages: ', totalPages);
-  console.log('current page', page);
-  if (page < totalPages) {
-    moreBtnShow();
-  }
+
   return data;
 }
 
-function renderGalleryMarkup(photoList) {
-  //creating markup
-  return photoList
-    .map(photo => {
-      return `
+function templateImage({ largeImageURL, webformatURL, tags }) {
+  return `
       <li class="gallery-item">
-        <a class="gallery-link" href="${photo.largeImageURL}">
+        <a class="gallery-link" href="${largeImageURL}">
           <img
             class="gallery-image"
-            src="${photo.webformatURL}"
-            data-source="${photo.largeImageURL}"
-            alt="${photo.tags}"
+            src="${webformatURL}"
+            data-source="${largeImageURL}"
+            alt="${tags}"
           />
         </a>
       </li>
       `;
-    })
-    .join('\n');
+}
+
+function templateImages(images) {
+  return images.map(templateImage).join('\n');
 }
 
 function loaderHide(loader) {
@@ -115,6 +119,15 @@ function moreBtnHide() {
 
 function moreBtnShow() {
   refs.moreWrapper.classList.remove('hidden');
+}
+
+//deciding to show more button or not
+function moreBtnCheck() {
+  if (page < totalPages) {
+    moreBtnShow();
+  } else {
+    moreBtnHide();
+  }
 }
 
 function createGallery(data) {
@@ -136,27 +149,17 @@ function createGallery(data) {
   }
 
   //if images are found, then rendering gallery
-  refs.galleryList.innerHTML = renderGalleryMarkup(data.hits);
+  refs.galleryList.innerHTML = templateImages(data.hits);
 
   //hiding css-load
   loaderHide(refs.searchLoader);
-  // if (gallery) {
-  //   console.log('refreshing gallery');
-  //   gallery.refresh();
-  // }
+
   initSimpleLightBox();
 }
 
 function initSimpleLightBox() {
-  //initializing simplelightbox
-  // gallery = null;
-  // if (gallery) {
-  //   gallery.refresh();
-  // gallery = null;
-  // }
   gallery = new SimpleLightbox('.gallery-list a', simplelightboxOptions);
   gallery.refresh();
-  // return gallery;
 }
 
 refs.moreBtn.addEventListener('click', onMoreClick);
@@ -176,8 +179,10 @@ async function onMoreClick(e) {
   try {
     page += 1;
     gallery.destroy();
-    const data = await fetchPhotos(searchText);
-    const markup = renderGalleryMarkup(data.hits);
+    const data = await getImages(searchText);
+    const markup = templateImages(data.hits);
+
+    moreBtnCheck;
     loaderHide(refs.moreLoader);
     refs.galleryList.insertAdjacentHTML('beforeend', markup);
     smoothScroll();
